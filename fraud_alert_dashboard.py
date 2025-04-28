5ca840c274dc15cd11c41a7eafd0262707fe53f2
Sharon Dev Saseendran <s5678sha@gmail.com>
2025-04-27 22:50:26 -0400

    changes

--- fraud_alert_dashboard.py
+++ fraud_alert_dashboard.py
@@ -149,4 +149,4 @@ app.layout = html.Div(
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
